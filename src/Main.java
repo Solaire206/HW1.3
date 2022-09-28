c92e6d1912df51e7b94ff01f2e317ceece0b9bab
Petr <petiauzunov1997481@gmail.com>
2022-09-28 17:40:22 +0400

    задание 4 и 5

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main (String[]args) {
+        //Задание 4
+        System.out.println("Задание №4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        // задание 5
+System.out.println("Задание 5");
+var frog = 3.5;
+System.out.println(frog);
+frog = frog * 10;
+System.out.println(frog);
+frog = frog / 3.5;
+System.out.println(frog);
+frog = frog + 4;
+System.out.println(frog);
+
+    }
+}
